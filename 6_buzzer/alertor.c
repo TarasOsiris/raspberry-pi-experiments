af86aaec4aa255ab91a1901ba566096d8a7490a6
Taras Leskiv <leskiv.taras@gmail.com>
2024-01-31 09:03:47 -1000

    wip

---
+++ 6_buzzer/alertor.c
@@ -0,0 +1,66 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pigpio.h>
+#include <time.h>
+#include <bits/time.h>
+#include <unistd.h>
+#include <math.h>
+
+#define buzzerPin 17
+#define buttonPin 18
+
+volatile sig_atomic_t signal_received = 0;
+
+void sigint_handler(int signal) {
+	signal_received = signal;
+}
+
+void alertor(int pin) {
+    int x;
+    double sinVal, toneVal;
+
+		gpioPWM(buzzerPin, 128);
+        gpioSetPWMfrequency(buzzerPin, 2000);
+         gpioSleep(PI_TIME_RELATIVE, 1, 0);
+
+
+    // for (x = 0; x < 360; x++) {
+    //     sinVal = sin(x * (M_PI / 180));
+    //     toneVal = 2000 + sinVal * 500;
+	// 	gpioPWM(buzzerPin, 128);
+    //     gpioSetPWMfrequency(buzzerPin, toneVal);
+    //     gpioSleep(PI_TIME_RELATIVE, 1, 0);
+    // }
+}
+
+void stopAlertor(int pin) {
+	gpioPWM(buzzerPin, 0);
+}
+
+int main() {
+	if (gpioInitialise() == PI_INIT_FAILED) {
+		printf("ERROR: Failed to initialize the GPIO interface.\n");
+		return 1;
+	}
+	printf("Press CTRL-C to exit.\n");
+	signal(SIGINT, sigint_handler);
+
+	gpioSetMode(buzzerPin, PI_OUTPUT);
+	gpioSetMode(buttonPin, PI_INPUT);
+	gpioSetPullUpDown(buttonPin, PI_PUD_UP);
+
+	while (!signal_received) {
+		if (gpioRead(buttonPin) == PI_LOW) {
+			printf("on.\n");
+			alertor(buzzerPin);
+		} else {
+			printf("off.\n");
+			stopAlertor(buzzerPin);
+		}
+	}
+
+	gpioTerminate();
+	printf("\n");
+	return 0;
+}
